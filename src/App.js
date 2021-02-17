96471728d84f1386b6b60bbf20ad2168826aacd5
Franyeliza <franyelizarodriguez@live.com>
2021-02-17 15:58:30 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React, { useState } from "react"
 import './App.css';
+import {BrowserRouter, Route, Switch} from "react-router-dom"
+import Dashboard from "./Components/Dashboard"
+import Preferences from "./Components/Preferences"
+import Login from "./Components/Login/Login"
 
 function App() {
+  const [token, setToken]= useState()
+  if(!token){
+    return <Login setToken={setToken}/>
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+      <Switch>
+        <Route path="/dashboard">
+          <Dashboard/>
+        </Route>
+         <Route path="/preferences">
+          <Preferences/>
+        </Route>
+      </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/Preferences.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Preferences =()=>{
+    return(
+    <div>
+        <h2>Preferences</h2>
+    </div>
+
+    )
+}
+
+export default Preferences;
